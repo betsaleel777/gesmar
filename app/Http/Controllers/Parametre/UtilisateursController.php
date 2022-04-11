56b5474fd353644ad9751e7e1c8f914bb560adde
Ahoussou Jean-Chris <shin.ahoussou@gmail.com>
2022-04-11 14:20:53 +0000

    permissions et authentification
    il reste à faire fonctionner l'assignation de permission directes et les chargé dans vue-gates

--- app/Http/Controllers/Parametre/UtilisateursController.php
+++ app/Http/Controllers/Parametre/UtilisateursController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Role;
 
 class UtilisateursController extends Controller
 {
@@ -24,10 +25,9 @@ class UtilisateursController extends Controller
 
     public function show(int $id)
     {
-        $user = User::withTrashed()->find($id);
-        $permissions = $user->getAllPermissions()->all();
-        $role = $user->getRoleNames();
-        return response()->json(['user' => $user, 'permissions' => $permissions, 'role' => $role]);
+        $user = User::with('roles', 'permissions')->withTrashed()->find($id);
+        $permissions = $user->getAllPermissions();
+        return response()->json(['user' => $user, 'permissions' => $permissions]);
     }
 
     public function store(Request $request)
@@ -40,7 +40,7 @@ class UtilisateursController extends Controller
         $user->deconnecter();
         $user->save();
         $message = "L'utilisateur $request->name a été crée avec succès.";
-        return response()->json(['message' => $message, 'request' => $request->all()]);
+        return response()->json(['message' => $message]);
     }
 
     public function profile(Request $request)
@@ -90,11 +90,64 @@ class UtilisateursController extends Controller
                 return response()->json(['message' => $message]);
             }
         }
-
     }
 
-    public function autoriser(Request $request)
+    public function autoriserDirect(Request $request)
     {
+        $user = User::with('roles')->find($request->id);
+        $rolePermissions = $user->getPermissionsViaRoles()->all();
+        $message = "";
+        // attribuer des permissions directes
+        $directPermissions = [];
+        if (count($rolePermissions) > 0) {
+            $ids = array_column($rolePermissions, 'id');
+            $directPermissions = array_filter($request->permissions, function ($permission) use ($ids) {
+                return !in_array($permission['id'], $ids);
+            }, ARRAY_FILTER_USE_BOTH);
+            if (count($directPermissions) > 0) {
+                $permissions = array_column($directPermissions, 'id');
+                $user->givePermissionTo([$permissions]);
+                $message .= "Les permissions ont été accordées directement à l'utilisateur $user->name";
+            }
+        }
+        return response()->json([
+            'message' => $message,
+            'directs' => $directPermissions,
+            'requete' => $request->all(),
+        ]);
+    }
+    public function autoriserByRole(Request $request)
+    {
+        $this->validate($request, ['role' => 'required']);
+        $user = User::with('roles')->find($request->id);
+        $rolePermissions = $user->getPermissionsViaRoles()->all();
+        $message = "";
+// attribuer des permissions directes
+        $directPermissions = [];
+        if (count($rolePermissions) > 0) {
+            $ids = array_column($rolePermissions, 'id');
+            $directPermissions = array_filter($request->permissions, function ($permission) use ($ids) {
+                return !in_array($permission['id'], $ids);
+            }, ARRAY_FILTER_USE_BOTH);
+            if (count($directPermissions) > 0) {
+                $permissions = array_column($directPermissions, 'id');
+                $user->givePermissionTo([$permissions]);
+                $message .= "Les permissions ont été accordées directement à l'utilisateur $user->name";
+            }
+        }
+//attribuer le role
+        $newRole = Role::find($request->role);
+        if (count($user->roles->all()) > 0) {
+            $user->removeRole($user->roles->first());
+        }
+        $user->assignRole($newRole);
+        $message .= "Le role $newRole->name, a été attribué avec succès à l'utilisateur $user->name";
+        return response()->json([
+            'message' => $message,
+            'directs' => $directPermissions,
+            'roles' => $rolePermissions,
+            'requete' => $request->all(),
+        ]);
 
     }
 
