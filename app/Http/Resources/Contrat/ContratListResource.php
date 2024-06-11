138ab4d65d14469766cff8ae5aee7d0f820ebad8
betsaleel777 <shin.ahoussou@gmail.com>
2024-06-11 15:54:45 +0000

    les contrats sans factures doivent être à jours

--- app/Http/Resources/Contrat/ContratListResource.php
+++ app/Http/Resources/Contrat/ContratListResource.php
@@ -3,8 +3,12 @@
 namespace App\Http\Resources\Contrat;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Exploitation\Contrat;
 use Illuminate\Support\Str;
 
+/**
+ * @property Contrat $resource
+ */
 class ContratListResource extends JsonResource
 {
     public function toArray($request): array
@@ -14,10 +18,10 @@ class ContratListResource extends JsonResource
             'code' => $this->code_contrat ?? $this->code,
             'created_at' => $this->created_at?->format('d-m-Y'),
             'status' => $this->whenAppended('status'),
-            'uptodate' => $this->whenNotNull($this->uptodate),
+            'uptodate' => $this->whenNotNull($this->getStatus()),
             'emplacement' => $this->whenLoaded('emplacement', fn () => $this->emplacement->code),
             'annexe' => $this->whenLoaded('annexe', fn () => $this->annexe->code),
-            'personne' => $this->whenLoaded('personne', fn () => Str::lower($this->personne->getAlias())),
+            'personne' => $this->whenLoaded('personne', fn () => Str::lower($this->personne->getFullname())),
             'equipements' => EquipementResource::collection($this->whenLoaded('equipements')),
         ];
     }
