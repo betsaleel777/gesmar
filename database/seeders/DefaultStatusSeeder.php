2773e08b0855ac491d0122465990df43bdb0edca
betsaleel777 <shin.ahoussou@gmail.com>
2024-08-28 15:49:01 +0000

    les couleurs de configuration

--- database/seeders/DefaultStatusSeeder.php
+++ database/seeders/DefaultStatusSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Architecture\Abonnement;
-use App\Models\Exploitation\Contrat;
 use App\Models\Exploitation\Personne;
 use Illuminate\Database\Seeder;
 
@@ -11,10 +9,6 @@ class DefaultStatusSeeder extends Seeder
 {
     public function run(): void
     {
-        $contrats = Contrat::withoutGlobalScopes()->get();
-        $contrats->each(fn($contrat) => $contrat->validate());
-        $abonnements = Abonnement::withoutGlobalScopes()->get();
-        $abonnements->each(fn($abonnement) => $abonnement->process());
         Personne::withExists(['contrats as contratFound' => fn($query) => $query->validated()])->withoutGlobalScopes()->chunk(50, function ($personnes) {
             $personnes->each(fn($personne) => $personne->contratFound ? $personne->client() : $personne->prospect());
         });
